f6250d9ee80ada9b65bb101c440a2b58314180c0
VuongVoVan97 <vuong011197@gmail.com>
2023-09-25 11:38:35 +0700

    Environment

---
+++ marid/src/main/java/com/hotel/marid/MaridApplication.java
@@ -0,0 +1,13 @@
+package com.hotel.marid;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MaridApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MaridApplication.class, args);
+	}
+
+}
---
+++ marid/src/test/java/com/hotel/marid/MaridApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hotel.marid;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MaridApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
